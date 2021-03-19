aae30fe00e88aca5d7cb3e4377586f376ffb55af
MyViewAndViews <mafasyf2@gmail.com>
2021-03-19 22:17:23 +0700

    Initial commit

---
+++ app/src/main/java/id/sch/smktelkom/fragmentproject/PageViewModel.java
@@ -0,0 +1,14 @@
+package id.sch.smktelkom.fragmentproject;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+public class PageViewModel extends ViewModel {
+    private MutableLiveData<String> mName = new MutableLiveData<>();
+    public void setName(String name) {
+        mName.setValue(name);
+    }
+    public LiveData<String> getName() {
+        return mName;
+    }
+}
